5d8baaad61fe47d8bb10cdf258721317387fc9fe
Vincent Saulys <76964816+vssaulys@users.noreply.github.com>
2023-06-19 16:01:30 -0400

    Updated for TFC presentation
    Cleaned up and simplified project

---
+++ ManagedkdbInsights/basic_tick/basictick_setup.py
@@ -0,0 +1,10 @@
+#
+# Define components of Setup
+#
+
+RDB_CLUSTER_NAME="RDB_basictickdb_TFC"
+HDB_CLUSTER_NAME="HDB_basictickdb_TFC"
+GW_CLUSTER_NAME ="GATEWAY_TFC"
+
+# Managed KX Database
+DB_NAME="basictickdb"
