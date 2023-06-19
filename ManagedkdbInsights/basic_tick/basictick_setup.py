11d2a0f4dc7dc22fc0954736467b2483284d606b
Vincent Saulys <76964816+vssaulys@users.noreply.github.com>
2023-06-19 17:52:24 -0400

    Update basictick_setup.py

--- ManagedkdbInsights/basic_tick/basictick_setup.py
+++ ManagedkdbInsights/basic_tick/basictick_setup.py
@@ -2,9 +2,9 @@
 # Define components of Setup
 #
 
-RDB_CLUSTER_NAME="RDB_basictickdb_TFC"
-HDB_CLUSTER_NAME="HDB_basictickdb_TFC"
-GW_CLUSTER_NAME ="GATEWAY_TFC"
+RDB_CLUSTER_NAME="RDB_basictickdb"
+HDB_CLUSTER_NAME="HDB_basictickdb"
+GW_CLUSTER_NAME ="GATEWAY_basictickdb"
 
 # Managed KX Database
 DB_NAME="basictickdb"
