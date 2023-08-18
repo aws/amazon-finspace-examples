6b49a842729a66b29fefecccc871ec1d6d0eb17b
Vincent Saulys <76964816+vssaulys@users.noreply.github.com>
2023-08-18 14:09:19 -0400

    Updated Basic Tick Example

--- ManagedkdbInsights/basic_tick/env.py
+++ ManagedkdbInsights/basic_tick/env.py
@@ -1,1 +1,30 @@
-../env.py
+ACCOUNT_ID="123456789012"
+ENV_ID="xdbujsadpl6owu7kyzxm66"
+
+VPC_ID="vpc-00c87e99505f8b5bc"
+SECURITY_GROUPS=["sg-0f98fb0accef94f9e"]
+SUBNET_IDS=["subnet-0cb15b7de6cb72151"] # in AZ_ID
+AZ_MODE="SINGLE"
+AZ_ID="use1-az4"
+
+#S3_BUCKET="kdb-demo-123456789012"
+S3_BUCKET="kdb-demo-123456789012-kms"
+
+S3_CODE_BUCKET="kdb-demo-069735623166"
+
+EXECUTION_ROLE=f"arn:aws:iam::{ACCOUNT_ID}:role/kdb-cluster-user"
+
+ENDPOINT_URL=None
+
+# USER
+KDB_USERNAME="bob"
+
+# TP
+TP=":172.31.22.143:5000"
+TP_HOSTFILE = "tickerplant"
+
+# Access Variables
+AWS_ACCESS_KEY_ID=""
+AWS_SECRET_ACCESS_KEY=""
+AWS_SESSION_TOKEN=""
+
