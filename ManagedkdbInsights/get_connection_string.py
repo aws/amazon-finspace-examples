61e56e077cec5bc99bc097ae3126fb86546465c5
Vincent Saulys <76964816+vssaulys@users.noreply.github.com>
2024-03-05 12:32:53 -0500

    Update get_connection_string.py
    Added 3 arguments for outputing the connection string information:
    string, symbol, parts
    
    string is as before (:tcps://vpce-...)
    symbol is q symbol notation ( `vpce-...)
    parts: HOST, PORT, USERNAME, and PASSWORD as separate items

--- ManagedkdbInsights/get_connection_string.py
+++ ManagedkdbInsights/get_connection_string.py
@@ -11,6 +11,10 @@ if __name__ == '__main__':
     parser.add_argument("-cluster",    "-c", help="Cluster Name", required=True)
     parser.add_argument("-username",   "-u", help="kdb Username", required=True)
 
+    parser.add_argument("-string",    "-str", help="print as string", default=False, action=argparse.BooleanOptionalAction)
+    parser.add_argument("-symbol",    "-sym", help="print as symbol", default=False, action=argparse.BooleanOptionalAction)
+    parser.add_argument("-parts",   "-parts", help="print parts", default=False, action=argparse.BooleanOptionalAction)
+
     args = parser.parse_args()
 
     session = boto3.Session(profile_name = args.profile)
@@ -25,23 +29,39 @@ if __name__ == '__main__':
                                         userName=args.username, 
                                         boto_session=session)
 
-    print("Single String")
-    print(conn_str)
+    if bool(args.string):
+        print()
+        print("Single String")
+        print(80*"-")
+        print(conn_str)
+        print(80*"-")
 
-    print(80*"-")
-    print("In Parts")
-    print(80*"-")
+    if bool(args.symbol):
+        print()
+        print("Symbol")
+        print(80*"-")
+        sym_parts = conn_str.split("/")
 
-    conn_parts = conn_str.split(":")
+        print(f"`{sym_parts[2]}")
+        print(80*"-")
 
-    host=conn_parts[2].strip("/")
-    port = int(conn_parts[3])
-    username=conn_parts[4]
-    password=conn_parts[5]
+    if bool(args.parts):
+        print()
+        print("In Parts")
+        print(80*"-")
 
-    print(f"""
+        conn_parts = conn_str.split(":")
+
+        host=conn_parts[2].strip("/")
+        port = int(conn_parts[3])
+        username=conn_parts[4]
+        password=conn_parts[5]
+
+        print(f"""
 HOST: {host}
 PORT: {port}
 USERNAME: {username}
 PASSWORD:
 {password}""")
+
+        print(80*"-")
